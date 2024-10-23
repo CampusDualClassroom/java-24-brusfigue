0974228cb0c14ae67726557d94d4af4051cd3f71
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-23 16:59:12 +0200

    Ejercicio24acabado

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,30 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer("Smith");
+        queue.offer("Montessori");
+        queue.offer("Peralta");
+        queue.offer("House");
+
+        return queue;
 
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
 
+        System.out.println(queue.peek()+ "\n");
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
 
+        Queue<String> createQueue = createQueue();
+        System.out.println("Elementos de la cola: ");
+        printAndEmptyQueue(createQueue);
     }
 
 }
